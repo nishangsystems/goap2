0d3f1a30454b4c79a1efd5d48a7bfeffdf357ccc
germanusk <germanuskeming@gmail.com>
2025-08-01 15:49:16 +0100

    ----

--- resources/views/student/online/fill_form.blade.php
+++ resources/views/student/online/fill_form.blade.php
@@ -734,7 +734,7 @@ $em_key = time().random_int(3000, 3099);
                         
                         <div class="col-sm-12 col-md-12 col-lg-12 py-4 mt-5 d-flex justify-content-center text-uppercase">
                             <a href="{{ route('student.application.start', [$step-0.5, $application->id]) }}" class="px-4 py-1 btn btn-lg btn-danger">{{ __('text.word_back') }}</a>
-                            <a href="{{ route('student.home') }}" class="px-4 py-1 btn btn-lg btn-success">{{ __('text.pay_later') }}</a>
+                            {{-- <a href="{{ route('student.home') }}" class="px-4 py-1 btn btn-lg btn-success">{{ __('text.pay_later') }}</a> --}}
                             @if(!$application->is_filled())<button type="submit" class="px-4 py-1 btn btn-lg btn-primary text-uppercase">{{ __('text.word_submit') }}</button>@endif
                         </div>
                     </div>
