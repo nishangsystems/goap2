6e2305c4f97ec8d226fedd84f0e1501ce896b029
germanusk <germanuskeming@gmail.com>
2025-10-17 15:42:59 +0100

    -----

--- resources/views/admin/printable2.blade.php
+++ resources/views/admin/printable2.blade.php
@@ -88,7 +88,7 @@
 </head>
 <body class="no-skin">
     <div class="" style="background-color: white;">
-        <img src="{{ asset('assets/images/avatars/letterhead.jpg') }}" style="width: 100%; max-height: auto; margin-bottom: 1rem;">
+        <img src="{{ asset('assets/images/avatars/lhead.png') }}" style="width: 100%; max-height: auto; margin-bottom: 1rem;">
         {{-- <div class="text-center text-black h3 py-2">{{$title ?? ''}}</div> --}}
         @yield('section')
         {{-- <img src="{{ asset('assets/images/footer.png') }}" style="width: 100%; max-height: 20rem; margin-top: 1rem;"> --}}
